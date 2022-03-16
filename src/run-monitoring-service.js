42cf4c9da3de7a82318592a79366cb43fbf10d8c
Jałokim <robimy_po_swojemu@o2.pl>
2022-03-16 23:44:20 +0100

    JobApi -> ApiCheckerJob, JobStreamr -> StreamrCheckerJob, two loops -> one loop

--- src/run-monitoring-service.js
+++ src/run-monitoring-service.js
@@ -3,8 +3,8 @@ const consola = require("consola");
 const config = require("../default-data-sources/redstone-avalanche.json");
 const { connectToRemoteMongo } = require("./db-connector");
 const { execute: executeEmailNotifierJob } = require("./notifiers/email-notifier-job");
-const JobApi = require("./source-monitoring/api-source-checker-job");
-const JobStreamr = require("./source-monitoring/streamr-checker-job");
+const ApiCheckerJob = require("./source-monitoring/api-source-checker-job");
+const StreamrCheckerJob = require("./source-monitoring/streamr-checker-job");
 const EVERY_10_SECONDS = "*/10 * * * * *";
 
 connectToRemoteMongo();
@@ -18,23 +18,21 @@ schedule.scheduleJob(EVERY_10_SECONDS, async () => {
 });
 
 
-logger.info("Starting Api source monitoring jobs");
-for (const source of (config.sources).filter((source) => source.type == "cache-layer")) {
+logger.info("Starting source monitoring jobs");
+for (const source of config.sources) {
   logger.info("Starting a new source checker job with scehdule: " + source.schedule);
   schedule.scheduleJob(source.schedule, async () => {
     logger.info("Source checker iteration started");
-    jobApi = new JobApi();
-    await jobApi.execute(source);
+
+    switch (source.type) {
+      case "streamr-storage":
+        StreamrChecker = new StreamrCheckerJob();
+        await StreamrChecker.execute(source);
+        break;
+      case "cache-layer":
+        ApiChecker = new ApiCheckerJob();
+        await ApiChecker.execute(source);
+        break;
+    }
   });
 }
-
-logger.info("Starting Streamr source monitoring jobs");
-for (const source of (config.sources).filter((source) => source.type == "streamr-storage")) {
-  logger.info("Starting a new source checker job with scehdule: " + source.schedule);
-  schedule.scheduleJob(source.schedule, async () => {
-    logger.info("Source checker iteration started");
-    jobStreamr = new JobStreamr();
-    await jobStreamr.execute(source);
-  });
-}
-
