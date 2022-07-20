7d3e9e45524c22ebbd27d7f9b0431de6c986b7f4
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-07-20 10:24:16 +0200

    feat: update warnings and errors notification (#24)

--- backend/src/jobs/notifier-job.ts
+++ backend/src/jobs/notifier-job.ts
@@ -5,7 +5,8 @@ import { Mail } from "../models/mail";
 import { notify as notifyByEmail } from "../notifiers/email-notifier-mailgun";
 import { notify as notifyByTelegram } from "../notifiers/telegram-notifier";
 
-const MIN_MAIL_INTERVAL = 3 * 3600 * 1000; // 3 hours
+const MIN_WARNING_MAIL_INTERVAL = 3 * 3600 * 1000; // 3 hours
+const MIN_ERROR_MAIL_INTERVAL = 1800 * 1000; // 0.5 hour
 const ERRORS_LIMIT = 5;
 
 const logger = consola.withTag("email-notifier-job");
@@ -13,92 +14,148 @@ const logger = consola.withTag("email-notifier-job");
 // Counts warnings and errors for the last period of time
 export const execute = async () => {
   const currentTimestamp = Date.now();
-  const minTimestamp = currentTimestamp - MIN_MAIL_INTERVAL;
+  const minWarningTimestamp = currentTimestamp - MIN_WARNING_MAIL_INTERVAL;
+  const minErrorTimestamp = currentTimestamp - MIN_ERROR_MAIL_INTERVAL;
 
-  const lastSentMailCount = await Mail.countDocuments({
-    timestamp: { $gte: minTimestamp },
+  const lastWarningSentMailCount = await Mail.countDocuments({
+    timestamp: { $gte: minWarningTimestamp },
   }).exec();
-  if (lastSentMailCount > 0) {
-    logger.info("Sent more than 1 mail lately. Mail sending skipped.");
-    return;
+
+  const lastErrorSentMailCount = await Mail.countDocuments({
+    timestamp: { $gte: minErrorTimestamp },
+  }).exec();
+
+  if (lastErrorSentMailCount === 0) {
+    handleErrorMessage(minErrorTimestamp, currentTimestamp);
   }
 
+  if (lastWarningSentMailCount === 0) {
+    handleWarningMessage(minWarningTimestamp, currentTimestamp);
+  }
+};
+
+const handleErrorMessage = async (
+  minErrorTimestamp: number,
+  currentTimestamp: number
+) => {
   const errorsCount = await Issue.countDocuments({
-    timestamp: { $gte: minTimestamp },
+    timestamp: { $gte: minErrorTimestamp },
     level: "ERROR",
   });
 
-  const warningsCount = await Issue.countDocuments({
-    timestamp: { $gte: minTimestamp },
-    level: "WARNING",
-  });
-
   const lastErrors = await Issue.find({
-    timestamp: { $gte: minTimestamp },
+    timestamp: { $gte: minErrorTimestamp },
     level: "ERROR",
   }).limit(ERRORS_LIMIT);
 
   const issuesAnalysis = await generateIssuesAnalysis(
-    minTimestamp,
+    minErrorTimestamp,
     currentTimestamp
   );
 
-  const message =
+  const message = generateErrorMessage(
+    errorsCount,
+    minErrorTimestamp,
+    currentTimestamp,
+    lastErrors,
+    issuesAnalysis
+  );
+
+  if (errorsCount > 0) {
+    await sendEmailNotification(
+      message,
+      currentTimestamp,
+      errorsCount,
+      "errors"
+    );
+    await sendTelegramNotification(errorsCount, "errors");
+  } else {
+    logger.info("No new notifications to send. Skipping...");
+  }
+};
+
+const handleWarningMessage = async (
+  minWarningTimestamp: number,
+  currentTimestamp: number
+) => {
+  const warningsCount = await Issue.countDocuments({
+    timestamp: { $gte: minWarningTimestamp },
+    level: "WARNING",
+  });
+
+  const message = generateWarningMessage(
+    warningsCount,
+    minWarningTimestamp,
+    currentTimestamp
+  );
+  if (warningsCount > 0) {
+    await sendEmailNotification(
+      message,
+      currentTimestamp,
+      warningsCount,
+      "warnings"
+    );
+    await sendTelegramNotification(warningsCount, "warnings");
+  } else {
+    logger.info("No new notifications to send. Skipping...");
+  }
+};
+
+const generateErrorMessage = (
+  errorsCount: number,
+  minTimestamp: number,
+  currentTimestamp: number,
+  lastErrors: Issue[],
+  issuesAnalysis: Awaited<ReturnType<typeof generateIssuesAnalysis>>
+) => {
+  return (
     "Hello devs!\n\n" +
     "Here is a short report from redstone-node-monitoring tool\n" +
     JSON.stringify(
       {
         errors: errorsCount,
-        warnings: warningsCount,
         mongoQueryForErrors: {
           timestamp: { $gte: minTimestamp, $lte: currentTimestamp },
           level: "ERROR",
-        },
-        mongoQueryForWarnings: {
-          timestamp: { $gte: minTimestamp, $lte: currentTimestamp },
-          level: "WARNING",
         },
         lastErrors,
         issuesAnalysis,
       },
       null,
       2
-    );
-
-  const shouldNotify = errorsCount > 0 || warningsCount > 0;
-  if (shouldNotify) {
-    await sendEmailNotification(
-      errorsCount,
-      warningsCount,
-      message,
-      currentTimestamp
-    );
-    await sendTelegramNotification(errorsCount, warningsCount);
-  } else {
-    logger.info("No new notifications to send. Skipping...");
-  }
+    )
+  );
 };
 
-const getEmailSubject = (errorsCount: number, warningsCount: number) => {
-  let subject = "";
-  if (errorsCount > 0) {
-    subject = `[RedStone node monitoring] - please check ${errorsCount} errors`;
-  } else if (warningsCount > 0) {
-    subject =
-      errorsCount > 0
-        ? `${subject} and ${warningsCount} warnings`
-        : `[RedStone node monitoring] - please check ${warningsCount} warnings`;
-  }
-  return subject;
+const generateWarningMessage = (
+  warningsCount: number,
+  minTimestamp: number,
+  currentTimestamp: number
+) => {
+  return (
+    "Hello devs!\n\n" +
+    "Here is a short report from redstone-node-monitoring tool\n" +
+    JSON.stringify(
+      {
+        warnings: warningsCount,
+        mongoQueryForWarnings: {
+          timestamp: { $gte: minTimestamp, $lte: currentTimestamp },
+          level: "WARNING",
+        },
+      },
+      null,
+      2
+    )
+  );
 };
 
 const sendEmailNotification = async (
-  errorsCount: number,
-  warningsCount: number,
   message: string,
-  currentTimestamp: number
+  currentTimestamp: number,
+  issueCount: number,
+  issueType: "warnings" | "errors"
 ) => {
-  const subject = getEmailSubject(errorsCount, warningsCount);
+  const subject = `[RedStone node monitoring] - please check ${issueCount} ${issueType}`;
   logger.info("Sending new email notification");
   const sentEmailReport = await notifyByEmail(subject, message);
   await new Mail({ timestamp: currentTimestamp }).save();
@@ -106,11 +163,17 @@ const sendEmailNotification = async (
 };
 
 const sendTelegramNotification = async (
-  errorsCount: number,
-  warningsCount: number
+  issueCount: number,
+  issueType: "warnings" | "errors"
 ) => {
-  logger.info("Sending new email notification");
-  const telegramMessage = `Errors: ${errorsCount}, Warnings: ${warningsCount}`;
+  logger.info("Sending new telegram notification");
+  const telegramMessage = `${capitalizeFirstLetter(issueType)}: ${issueCount}`;
   await notifyByTelegram(telegramMessage);
   logger.info("Telegram message sent: " + JSON.stringify(telegramMessage));
+};
+
+const capitalizeFirstLetter = (stringToCapitalize: string) => {
+  return (
+    stringToCapitalize.charAt(0).toUpperCase() + stringToCapitalize.slice(1)
+  );
 };
